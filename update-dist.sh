7b387418081b7e8d6b63694cdc4c37fe6d9adbac
Brian Katzung <briank@kappacs.com>
2026-06-24 21:51:07 -0500

    Added update-dist.sh script to update only stale minified runtime files.

---
+++ update-dist.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+stale () {
+	cd src/runtime
+	for f in *.js
+	do find $f -newer ../../dist/runtime-min/$f
+	done
+}
+
+for f in `stale`
+do deno run -A npm:esbuild src/runtime/$f --minify --outdir=dist/runtime-min --format=esm --sourcemap=external
+done
